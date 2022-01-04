cdc8ccde3c9e3882871576234f2a108249ad8fa1
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2022-01-04 17:52:52 +0000

    feat: add global layout

---
+++ pages/add.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+function Add() {
+  return (
+    <div>
+      <Layout title="Envanter Ekle" />
+
+      <main className="container"></main>
+    </div>
+  );
+}
+
+export default Add;
